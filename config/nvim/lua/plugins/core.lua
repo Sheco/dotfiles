2c9cb60e1aea7fe87b14131356ea1ba12cc244a2
Sergio Durán <sergio@tiene.info>
2024-10-09 20:47:34 -0600

    Enable inlay hints again, why not?

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -23,11 +23,5 @@ return {
 	{ "zeek/vim-zeek", enabled = true; },
 	{ "folke/flash.nvim", enabled = false },
 	{ "windwp/nvim-ts-autotag", enabled = false },
-	{
-		"nvim-lspconfig",
-		opts = {
-			inlay_hints = { enabled = false },
-		}
-	}
 }
 
