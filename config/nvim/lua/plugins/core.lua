0f48dfa49ad2305eb37b66b28ed9941b9d4e9ebb
Sergio Durán <sergio@tiene.info>
2024-10-11 07:31:19 -0600

    Use the indentation lines again, why not?

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -16,7 +16,7 @@ lspconfig.zeek.setup{}
 
 return {
 	{ "echasnovski/mini.indentscope", enabled = false, },
-	{ "lukas-reineke/indent-blankline.nvim", enabled = false, },
+--	{ "lukas-reineke/indent-blankline.nvim", enabled = false, },
 	{ "folke/noice.nvim", enabled = false },
 	{ "echasnovski/mini.pairs", enabled = false },
 	{ "zeek/vim-zeek", enabled = true; },
