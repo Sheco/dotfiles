02e840f7a43746ef4a49f021a41692c7421ec3a9
Sergio Durán <sergio@tiene.info>
2026-04-07 08:45:14 -0600

    remove autopairs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -323,11 +323,9 @@ require("which-key").setup({
 
 -- INFO: utility plugins
 vim.pack.add({
-  "https://github.com/windwp/nvim-autopairs",   -- auto pairs
   "https://github.com/folke/todo-comments.nvim" -- highlight TODO/INFO/WARN comments
 }, { confirm = false })
 
-require("nvim-autopairs").setup()
 require("todo-comments").setup()
 
 -- extra plugins
