95ace8145bb8f0a924d0581585842f43dfd85ac4
Sergio Durán <sergio@tiene.info>
2026-07-02 09:43:02 -0600

    Improve git keymaps from gitsigns

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -353,13 +353,72 @@ require('neo-tree').setup({
 })
 
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", {desc="Neogit"})
-vim.keymap.set("n", "<leader>gs", require("gitsigns").stage_hunk)
-vim.keymap.set("v", "<leader>gs", function()
-  require("gitsigns").stage_hunk({
-    vim.fn.line("."),
-    vim.fn.line("v"),
-  })
-end)
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end, { desc = "Git next hunk"})
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end, { desc = "Git prev hunk"})
+
+    -- Actions
+    map('n', '<leader>gs', gitsigns.stage_hunk, { desc = "Git stage hunk"})
+    map('n', '<leader>gr', gitsigns.reset_hunk, { desc = "Git reset hunk"})
+
+    map('v', '<leader>gs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end, { desc = "Git stage hunk"})
+
+    map('v', '<leader>gr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end, { desc = "Git reset hunk"})
+
+    map('n', '<leader>gS', gitsigns.stage_buffer, { desc = "Git stage buffer"})
+    map('n', '<leader>gR', gitsigns.reset_buffer, { desc = "Git reset buffer"})
+    map('n', '<leader>gp', gitsigns.preview_hunk, { desc = "Git preview hunk"})
+    map('n', '<leader>gi', gitsigns.preview_hunk_inline, { desc = "Git preview hunk inline"})
+
+    map('n', '<leader>gb', function()
+      gitsigns.blame_line({ full = true })
+    end, { desc = "Git blame line"})
+
+    map('n', '<leader>gd', gitsigns.diffthis, { desc = "Git diff this"})
+
+    map('n', '<leader>gD', function()
+      gitsigns.diffthis('~')
+    end, { desc = "Git diff this (~)"})
+
+    map('n', '<leader>gQ', function() gitsigns.setqflist('all') end, { desc = "Git show changes (all)"})
+    map('n', '<leader>gq', gitsigns.setqflist, { desc = "Git show changes"})
+
+    -- Toggles
+    map('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = "Git toggle current line blame"})
+    map('n', '<leader>gw', gitsigns.toggle_word_diff, { desc = "Git toggle word diff"})
+
+    -- Text object
+    map({'o', 'x'}, 'gh', gitsigns.select_hunk, { desc = "Git select hunk"})
+  end
+}
 
 -- INFO: terminal
 vim.keymap.set("n", "<leader>t", function()
